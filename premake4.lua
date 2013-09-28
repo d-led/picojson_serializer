7ceb92213c20d00c0bc45244e444dd5b1fec3bdb
d-led <dmlled@yahoo.com>
2013-09-28 13:46:47 +0200

    switch to premake build

---
+++ premake4.lua
@@ -0,0 +1,76 @@
+local OS=os.get()
+
+local definitions = {
+	dir = {
+		linux = "ls",
+		windows = "dir"
+	}
+}
+
+local cfg={}
+
+for i,v in pairs(definitions) do
+ cfg[i]=definitions[i][OS]
+end
+
+-- Apply to current "filter" (solution/project)
+function DefaultConfig()
+	location "Build"
+	configuration "Debug"
+		defines { "DEBUG", "_DEBUG" }
+		objdir "Build/obj"
+		targetdir "Build/Debug"
+		flags { "Symbols" }
+	configuration "Release"
+		defines { "RELEASE" }
+		objdir "Build/obj"
+		targetdir "Build/Release"
+		flags { "Optimize" }
+	configuration "*" -- to reset configuration filter
+end
+
+function CompilerSpecificConfiguration()
+	configuration {"xcode*" }
+		postbuildcommands {"$TARGET_BUILD_DIR/$TARGET_NAME"}
+
+	configuration {"gmake"}
+		postbuildcommands  { "$(TARGET)" }
+		buildoptions { "-std=c++0x" }
+
+	configuration {"codeblocks" }
+		postbuildcommands { "$(TARGET_OUTPUT_FILE)"}
+
+	configuration { "vs*"}
+		postbuildcommands { "\"$(TargetPath)\"" }
+end
+
+-- A solution contains projects, and defines the available configurations
+local sln=solution "picojson_serializer"
+    location "Build"
+	sln.absbasedir=path.getabsolute(sln.basedir)
+	configurations { "Debug", "Release" }
+	platforms { "native" , "x32", "x64" }
+	includedirs {
+		sln.basedir,
+		path.join(sln.basedir,"Catch/single_include"),
+		path.join(sln.basedir,"picojson"),
+		path.join(sln.basedir,"test")
+	}
+	vpaths {
+		["Headers"] = "**.h",
+		["Sources"] = {"**.cc", "**.cpp"},
+	}
+
+----------------------------------------------------------------------------------------------------------------
+
+local tests=project "test"
+	local basedir="test"
+	kind "ConsoleApp"
+	DefaultConfig()
+	language "C++"
+	files {
+		path.join(basedir,"**.cpp"),
+		path.join(basedir,"**.h"),
+		path.join(basedir,"../*.h")
+	}
+	CompilerSpecificConfiguration()
