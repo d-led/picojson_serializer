36071001ab7d99494bcdb3f27cb863482cbc14b7
d-led <dmlled@yahoo.com>
2014-01-29 21:59:35 +0100

    custom serializer example

---
+++ test/custom_converter_example.cpp
@@ -0,0 +1,51 @@
+#include <catch.hpp>
+
+#include "../picojson_serializer.h"
+#include "test_helpers.h"
+
+namespace {
+    struct Example {
+        enum Status {
+			NONE = 0,
+			SOME,
+			SOME_OTHER
+		};
+
+		Status status;
+
+        friend class picojson::convert::access;
+        template<class Archive>
+        void json(Archive & ar)
+        {
+            ar & picojson::convert::member("status", status);
+        }
+    };
+}
+
+namespace picojson {
+	namespace convert {
+
+		template<> struct value_converter<Example::Status> {
+			static value to_value(Example::Status v) { return value(static_cast<double>(v)); }
+			static void from_value(value const& ov, Example::Status& v) { if ( ov.is<double>() ) v = Example::Status(static_cast<int>(ov.get<double>())); }
+		};
+
+
+	}
+}
+
+TEST_CASE("custom serializer example") {
+	Example e;
+
+	e.status=Example::NONE;
+    picojson::value ev = picojson::convert::to_value(e);
+	CHECK(has<double>(ev, "status", 0));
+
+	e.status=Example::SOME;
+    ev = picojson::convert::to_value(e);
+	CHECK(has<double>(ev, "status", static_cast<int>(Example::SOME)));
+
+	e.status=Example::SOME_OTHER;
+    ev = picojson::convert::to_value(e);
+	CHECK(has<double>(ev, "status", static_cast<int>(Example::SOME_OTHER)));
+}
